0a4ab49a6019e213d16a19c841306ef188cc4b68
Anandhu_P_A <anandhupa131@gmail.com>
2023-05-03 20:59:42 +0530

    react-app-created @ folder with name frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import Todo from "./components/dummy/Todo"
+import Header from "./components/header/Header";
+
+function App() {
+  return (
+    <> <Header/>
+   
+    </>
+  
+  );
+}
+
+export default App;
---
+++ frontend/src/components/dummy/Todo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Text}from "@chakra-ui/react"
+import {Heading,VStack,IconButton} from '@chakra-ui/react'
+import { FaSun } from "react-icons/fa";
+function Todo() {
+  return (
+    <div>sdkf</div>
+  )
+}
+
+export default Todo
